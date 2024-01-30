94522da662544fad121def5b5d2e9d98e3940fed
Aimable01 <aimable.kwizera14@gmail.com>
2024-01-30 20:23:33 +0200

    Make the user schema, hash the password and define the function to compare the passwords after hashing them

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    username:{type:String, unique: true, required: true},
+    email:{type:String, required:true},
+    password:{type:String, required:true}
+})
+
+//hash the password
+userSchema.pre('save', async function(next){
+    const user = this
+    if(!user.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+    user.password = hash
+    next()
+})
+
+//compare the passwords
+userSchema.methods.comparePassword = async function(candidatePassword){
+    return comparePassword(candidatePassword, this.password)
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = User;
